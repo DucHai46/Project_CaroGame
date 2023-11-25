f60e1eb5e96b0ad72b8eb1618ccf3a48ae7da7dc
Bùi Đức Hải <haccongtu1221@gmail.com>
2023-11-25 23:31:53 +0700

    90% FE

--- FE_Caro/src/app/layout/lobby/room/room.component.ts
+++ FE_Caro/src/app/layout/lobby/room/room.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room } from 'src/app/models/Room.model';
 
 @Component({
   selector: 'app-room',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./room.component.css']
 })
 export class RoomComponent {
+  reset() {
+    this.squares.fill('0');
+  }
+  tick(index: number) {
+    if (this.squares[index] === '0') {
+      this.squares[index] = 'x';
+    }
+  }
+  room: Room = { "Player1": "Bui Duc Hai", "Player2": "Pham Hong Quan", "ChessBoard_state": "000000000", "Score_1": 0, "Score_2": 0, "History_Chat": "" }
+  squares: string[] = this.room.ChessBoard_state.split('');
 
 }
